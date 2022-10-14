3b4530fa9c5b611000c6069727ca749c8f9bbbf5
Ayouba Anrezki <anrezkia531@gmail.com>
2022-10-14 20:24:39 +0200

    the StegCrypt application

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-"""
-
-import random, string
-
-rondomSecure = random.SystemRandom()
-"".join([random.choice(string.printable) for _ in range(24)])
-
-"""
-
+import os
 SECRET_KEY = "lp |i(.'F<sfuEbCVx\n3,RDt"
+
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
