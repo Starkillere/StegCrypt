4c873c1095215ed9cbe7c85191329b42989d75b6
Ayouba Anrezki <anrezkia531@gmail.com>
2022-10-17 14:35:38 +0200

    the application

--- StegCryptapp/views.py
+++ StegCryptapp/views.py
@@ -431,6 +431,8 @@ def page_on(error):
 @app.route("/show-user", methods=["GET"])
 def show_user():
     if "CONNECTED" in session and session['USER_NAME'] == "Administrateur1":
+        user_manager.delt_user(2, User, db)
+        data_manager.delt_user_data(2, PasswordManager, db)
         users = User.query.order_by(User.date_added)
         users_template = ""
         for user in users:
