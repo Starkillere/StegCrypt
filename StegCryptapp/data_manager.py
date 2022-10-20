4b0b9854880efac17bc1e802f8f3e2f2b9822143
Ayouba Anrezki <anrezkia531@gmail.com>
2022-10-20 15:01:20 +0200

    app

--- StegCryptapp/data_manager.py
+++ StegCryptapp/data_manager.py
@@ -47,11 +47,8 @@ def allowed_file(filename, allwed_extensions):
 def is_image(filename:str):
     try:
         image = Image.open(filename)
-        print("la1")
         image.verify()
-        print("la2")
     except:
-        print("la")
         return False
     return True
 
